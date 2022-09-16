8519f1ee033c7f646953feea2cc4b65ca5fa879a
madonchik123 <68397448+madonchik123@users.noreply.github.com>
2022-09-16 08:53:35 +0300

    Update Event.lua

--- Event.lua
+++ Event.lua
@@ -1,1 +1,205 @@
---
+local ToUse = "Genos [Overdrive]" -- must be a hill unit
+local Distance = 30
+repeat wait() until game.Loaded
+repeat wait() until game.Players.LocalPlayer.Character
+repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+--Functions
+local LoadAntiAfk = function()
+    spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
+    end)
+end
+local AutoRejoin = function(Time)
+     spawn(function()
+        while wait(Time) do
+            local tps = game:GetService("TeleportService")
+            local player = game.Players.LocalPlayer
+            local gameid = game.PlaceId
+
+            tps:Teleport(gameid,player)
+        end
+    end) 
+end    
+local CheckIfInLobby = function()
+    return game:GetService("ReplicatedStorage"):WaitForChild("Lobby").Value
+end
+local function GetAmountOfItemsIntable(tbl)
+    local items = 0
+    for index, value in pairs(tbl)do
+        if typeof(value) == "table" then -- if the value is a table we reiterate to get the number of items in it
+            items = items + getNumberOfItems(value) -- you can add 1 if you want to count the table aswell
+        else
+            items = items + 1
+        end
+    end
+    return items
+end
+local GetAmountOfItemsInModel = function(Model)
+    local Amount = 0
+    for i,v in pairs(Model:GetChildren()) do
+        Amount = Amount + 1
+    end
+    return Amount
+end
+local GetAmountOfTowersPlaced = function(TowerName)
+    local Towers = 0
+    local Tower = TowerName or ToUse
+    for i,v in pairs(game.Workspace:WaitForChild("Unit"):GetChildren()) do
+        if v.Owner.Value == game.Players.LocalPlayer and v.Name == Tower then
+            Towers = Towers + 1
+        end
+    end
+    return Towers
+end
+local GetCFrameToPlace = function(Type,WhichOne)
+    local CurrentHill = 0
+    local ToFind = WhichOne or 1
+    if Type == "Hill" then
+        for i,v in pairs(game:GetService("Workspace"):WaitForChild("Placeable").Hill:GetChildren()) do
+            CurrentHill = CurrentHill + 1
+            if v.Name == "Hill" and v:FindFirstChild("Hill_Part") then
+                if CurrentHill == ToFind and v.Part.CFrame ~= nil and Distance > (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-v.Hill_Part.Position).magnitude then
+                    return v.Hill_Part.CFrame
+                end
+            elseif v.Name == "Hill" and v:FindFirstChild("Part") then
+                if CurrentHill == ToFind and v.Part.CFrame ~= nil and Distance > (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-v.Part.Position).magnitude then
+                    return v.PrimaryPart.CFrame + Vector3.new(0,5,0)
+                end
+            elseif v.Name == "hill" and v:FindFirstChild("Rock") then
+                if CurrentHill == ToFind and v.Part.CFrame ~= nil and Distance > (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-v.Rock.Position).magnitude then
+                    return v.Rock.CFrame
+                end
+            elseif v.Name == "Land" and v:FindFirstChild("Part") then
+                if CurrentHill == ToFind and v.Part.CFrame ~= nil and Distance > (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-v.Part.Position).magnitude then
+                    return v.Part.CFrame
+                end
+            end
+        end
+    end
+end
+local GetCurrentTimeLeft = function()
+    local HUD = game.Players.LocalPlayer.PlayerGui:WaitForChild("HUD")
+    local timer = string.gsub(HUD:WaitForChild("EventFrame"):WaitForChild("ChallengeTimer").Text,"Challenge Changes In: ","")
+    local timervalue = string.gsub(timer,"m","")
+    return tonumber(timervalue)
+end
+local GetCurrentEventChallenge = function()
+    local HUD = game.Players.LocalPlayer.PlayerGui:WaitForChild("HUD")
+    local Event = string.gsub(HUD.EventFrame.Challenge.Milestone1.Text,"Kill Enemies on ","")
+    local Event2 = string.gsub(Event,"<u>","")
+    local Event3 = string.gsub(Event2,"</u>","")
+    return Event3
+end
+local JoinCurrentChallenge = function()
+    if GetCurrentTimeLeft() == nil then return end
+    if GetCurrentTimeLeft() > 1 then
+        if string.find(GetCurrentEventChallenge(),"Trial") then
+            local String = string.gsub(GetCurrentEventChallenge(),"Trial","")
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game:GetService("Workspace").Queue["Exam"..String].ExamDoor,0)
+            wait()
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game:GetService("Workspace").Queue["Exam"..String].ExamDoor,1)
+        elseif string.find(GetCurrentEventChallenge(),"Raid")  then
+            local String = string.gsub(GetCurrentEventChallenge(),"Raid","")
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game:GetService("Workspace").Queue["Raid "..String].ExamDoor,0)
+            wait()
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game:GetService("Workspace").Queue["Raid "..String].ExamDoor,1)
+        end
+    end
+end
+local ChangeSpeed = function(Speed)
+    if Speed == "2x" then
+        local args = {
+            [1] = "SpeedChange",
+            [2] = true
+        }
+
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes").Input:FireServer(unpack(args))
+    elseif Speed == "1x" then
+        local args = {
+            [1] = "SpeedChange",
+            [2] = false
+        }
+
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes").Input:FireServer(unpack(args))
+    end
+end
+local ChangeMode = function(Mode)
+    if Mode == "Normal" then
+        local args = {
+            [1] = "VoteGameMode",
+            [2] = Mode
+        }
+
+        game:GetService("ReplicatedStorage").Remotes.Input:FireServer(unpack(args))
+    elseif Mode == "Extreme" then
+        local args = {
+            [1] = "VoteGameMode",
+            [2] = Mode
+        }
+
+        game:GetService("ReplicatedStorage").Remotes.Input:FireServer(unpack(args))
+    end
+end
+local GetTower = function(TowerName)
+    local Towers = {}
+    for i,v in pairs(game.Workspace.Unit:GetChildren()) do
+        if v.Owner.Value == game.Players.LocalPlayer then
+            table.insert(Towers,v)
+        end
+    end
+    return Towers
+end
+local PlaceTower = function(TowerName,CFrame)
+    if CFrame == nil then return end
+    local args = {
+        [1] = "Summon",
+        [2] = {
+            ["Rotation"] = 0,
+            ["cframe"] = CFrame,
+            ["Unit"] = TowerName
+        }
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes").Input:FireServer(unpack(args))
+end
+local UpgradeTower = function()
+    pcall(function()
+        local TowerTable = GetTower()
+        local Tower = TowerTable[math.random(1,GetAmountOfItemsIntable(TowerTable))]
+        if Tower.UpgradeTag.Value ~= Tower.MaxUpgradeTag.Value then
+            local args = {
+                [1] = "Upgrade",
+                [2] = Tower
+            }
+
+            game:GetService("ReplicatedStorage").Remotes.Input:FireServer(unpack(args))
+        end
+    end)
+end
+wait(4)
+AutoRejoin(900)
+ChangeSpeed("2x")
+ChangeMode("Normal")
+if CheckIfInLobby() then
+    wait(3)
+    LoadAntiAfk()
+    spawn(function()
+        while wait() do
+            JoinCurrentChallenge()
+        end
+    end)
+else
+    LoadAntiAfk()
+    wait(2)
+    spawn(function()
+        while wait() do
+            if GetAmountOfTowersPlaced(ToUse) < 2 then
+                for i = 1,GetAmountOfItemsInModel(game.Workspace.Placeable.Hill) do
+                    PlaceTower(ToUse,GetCFrameToPlace("Hill",i))
+                end
+            end
+            wait(0.5)
+            UpgradeTower()
+        end
+    end)
+end
